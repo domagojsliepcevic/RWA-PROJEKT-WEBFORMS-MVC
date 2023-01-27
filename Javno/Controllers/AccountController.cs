448f2bdd5dbf85f454a0f6bcbdc4dcda5e0cd9d3
Domagoj Sliepčević <dsliepc@racunarstvo.hr>
2023-01-27 20:10:10 +0100

    Login view and functionality

--- Javno/Controllers/AccountController.cs
+++ Javno/Controllers/AccountController.cs
@@ -1,17 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using rwaLib.Models;
+using rwaLib.Utils;
+using rwaLib.DAL;
 using System.Web.Mvc;
+
 
 namespace Javno.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
-        public ActionResult Index()
+
+        public UserRepository _userRepository = new UserRepository();
+
+
+        public ActionResult LogIn()
+        {
+            var model = new User();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult LogIn(User user)
+        {
+
+            var authuser = _userRepository.AuthUser(user.Email, Cryptography.HashPassword(user.PasswordHash));
+            if (authuser != null)
+            {
+                Session["Email"] = authuser.Email;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Error";
+            }
+
+            return View();
+        }
+
+        public ActionResult Registration()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(User user)
+        {
+            return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction(actionName: "Index", controllerName: "Home");
         }
     }
 }
