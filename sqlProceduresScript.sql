f97640f505ce2c8ad62aaa8395f1f07fffc0e5e8
Domagoj Sliepčević <dsliepc@racunarstvo.hr>
2023-01-27 23:16:42 +0100

    Updated proc

--- sqlProceduresScript.sql
+++ sqlProceduresScript.sql
@@ -386,3 +386,58 @@ INSERT INTO AspNetUsers(Guid,CreatedAt,Email,EmailConfirmed,PasswordHash,PhoneNu
 VALUES(NEWID(),GETDATE(),@Email,1,@Passwordhash,@PhoneNumber,1,0,0,@UserName,@Address)
 END
 GO
+
+
+CREATE OR ALTER PROCEDURE SearchApartments
+	@rooms int = null,
+	@adults int = null,
+	@children int = null,
+	@destination int = null,
+	@order int = null
+AS
+
+	SELECT
+		a.Id,
+		a.[Name],
+		StarRating = (
+			SELECT avg(Stars) 
+			FROM ApartmentReview ar 
+			WHERE ar.ApartmentId = a.Id),
+		CityName = c.[Name],
+		BeachDistance,
+		TotalRooms,
+		MaxAdults,
+		MaxChildren,
+		Price,
+		RepresentativePicturePath = (
+			SELECT TOP 1 ap.[Path]
+			FROM dbo.ApartmentPicture ap
+			WHERE ap.ApartmentId = a.Id
+			ORDER BY ap.IsRepresentative DESC, Id)			
+		--TagId = t.Id,
+		--TagName = t.[Name]
+	FROM 
+		dbo.Apartment a
+		LEFT JOIN City c ON c.Id = a.CityId
+		LEFT JOIN dbo.ApartmentPicture ap ON ap.ApartmentId = a.Id
+		--JOIN TaggedApartment ta ON ta.ApartmentId = a.Id
+		--JOIN Tag t ON t.Id = ta.TagId
+	WHERE 
+		(@rooms IS NULL OR @rooms IS NOT NULL AND a.TotalRooms >= @rooms)
+		AND 
+		(@adults IS NULL OR @adults IS NOT NULL AND a.MaxAdults >= @adults)
+		AND 
+		(@children IS NULL OR @children IS NOT NULL AND a.MaxChildren >= @children)
+		AND 
+		(@destination IS NULL OR @destination IS NOT NULL AND a.CityId = @destination)
+		AND
+		ap.DeletedAt IS NULL
+	ORDER BY 
+        CASE
+            WHEN @order is null THEN a.Id
+            WHEN @order = 1 THEN TotalRooms
+            WHEN @order = 2 THEN MaxAdults
+            WHEN @order = 3 THEN MaxChildren
+            WHEN @order = 4 THEN Price
+        END
+GO
