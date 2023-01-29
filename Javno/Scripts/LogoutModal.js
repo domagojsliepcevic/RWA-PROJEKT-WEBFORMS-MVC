4bd0c891d50e3cf21731df1b1058dc3c12bae5d3
Domagoj Sliepčević <dsliepc@racunarstvo.hr>
2023-01-29 19:57:12 +0100

    Logout modal window added

---
+++ Javno/Scripts/LogoutModal.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#openModal').click(function () {
+        $('#myModal').show();
+    });
+});
+$(document).ready(function () {
+    $('#closeModal').click(function () {
+        $('#myModal').hide();
+    });
+});
+
+$(document).ready(function () {
+    $('#btnSubmit').click(function () {
+
+        window.location.href = 'Account/Logout';
+
+    });
+});
